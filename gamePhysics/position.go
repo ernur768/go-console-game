c09c3f7197c294d7dcb94ad783114a653e23a713
Yernur <220776@astanait.edu.kz>
2023-11-01 23:32:02 +0600

    fixed position
    * added a limits on the screen borders

--- gamePhysics/position.go
+++ gamePhysics/position.go
@@ -1,5 +1,10 @@
 package gamePhysics
 
+const (
+	boundX = 160
+	boundY = 28
+)
+
 type Position struct {
 	X int
 	Y int
@@ -10,17 +15,29 @@ func NewPosition(x, y int) Position {
 }
 
 func (p *Position) StepUp() {
+	if p.Y <= 1 {
+		return
+	}
 	p.Y = p.Y - 1
 }
 
 func (p *Position) StepDown() {
+	if p.Y >= boundY {
+		return
+	}
 	p.Y = p.Y + 1
 }
 
 func (p *Position) StepLeft() {
+	if p.X <= 1 {
+		return
+	}
 	p.X = p.X - 1
 }
 
 func (p *Position) StepRight() {
+	if p.X >= boundX {
+		return
+	}
 	p.X = p.X + 1
 }
